6b5efde89f01f6140e9e8278d595c47851b6b73b
rraadd88 <rraadd_8@hotmail.com>
2018-10-14 20:57:38 -0400

    fix

--- test_datasets.py
+++ test_datasets.py
@@ -8,14 +8,14 @@ def test_species(host='saccharomyces_cerevisiae',genomeassembly='R64-1-1'):
     import yaml 
     cfg=yaml.load(open('common/configuration.yml','r'))
     # clone test_beditor 
-    if not exists('test_beditor'):
-        runbashcmd('git clone https://github.com/rraadd88/test_beditor.git',test=True)
-    else:
-        runbashcmd('cd test_beditor;git pull',test=True)
+#     if not exists('test_beditor'):
+#         runbashcmd('git clone https://github.com/rraadd88/test_beditor.git',test=True)
+#     else:
+#         runbashcmd('cd test_beditor;git pull',test=True)
     cfg['genomeassembly']=genomeassembly
-    com=f"source activate beditor;cd test_beditor;python make_datasets.py --species {host} --genomerelease {cfg['genomerelease']} --genomeassembly {cfg['genomeassembly']}"
+    com=f"source activate beditor;python make_datasets.py --species {host} --genomerelease {cfg['genomerelease']} --genomeassembly {cfg['genomeassembly']}"
     runbashcmd(com,test=True)
-    for datasetd in glob('test_beditor/dataset_*'):
+    for datasetd in glob('dataset_*'):
         for cfgp in glob(f"{datasetd}/*.yml"):
             cfgp=basename(cfgp)
             com=f"source activate beditor;cd {datasetd};beditor --cfg {cfgp}"
